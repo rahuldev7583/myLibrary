02b87b6119221c396ee97e9af80a4fcf7221fba0
Rahul Dev <rahuldev7583@gmail.com>
2022-10-04 13:26:21 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('p').addEventListener("click", addBook);
+document.querySelector('h3').addEventListener("click", addBook);
 document.getElementById('submit').addEventListener("click", submit);
 
  let myLibrary = [];
@@ -27,34 +27,38 @@ function submit(){
   document.getElementById("form").style.display = "none";
   document.getElementById("main").style.display = "inline-block";
   document.getElementById("books").style.display = "inline";
-  let title = document.getElementById("title").value;
+  let title = '"' + document.getElementById("title").value + '"';
   let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
+  let pages = document.getElementById("pages").value + " pages";
  let read = document.getElementById("read").checked;
  newArr.push(addBookToLibrary(title, author, pages, read));
  console.log(newArr[0].info());
  myLibrary.push(newArr[0]);
 newArr = [];
- 
+let indexOfClass = myLibrary.length - 1;
+ const bookDiv = document.createElement('div');
+ bookDiv.classList.add("bookDiv");
+ bookDiv.innerHTML = null;
+document.getElementById("books").appendChild(bookDiv);
  const bookTitle = document.createElement('p');
  bookTitle.innerHTML = title;
-document.getElementById("books").appendChild(bookTitle);
+document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(bookTitle);
 const bookAuthor = document.createElement('p');
  bookAuthor.innerHTML = author;
-document.getElementById("books").appendChild(bookAuthor);
+document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(bookAuthor);
 const bookPages = document.createElement('p');
  bookPages.innerHTML = pages;
-document.getElementById("books").appendChild(bookPages);
+document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(bookPages);
 const complete = document.createElement('button');
 //complete.id = "complete";
-complete.classList.add("complete");
+//complete.classList.add("complete");
 if(read){
   complete.innerHTML = "completed";
-  document.getElementById("books").appendChild(complete);
+  document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(complete);
   
 } else{
   complete.innerHTML = "Not read yet";
- document.getElementById("books").appendChild(complete);
+ document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(complete);
 }
 //console.log(complete);
  complete.addEventListener("click", ()=>{
@@ -62,11 +66,11 @@ if(read){
   for(let i = 0; i < myLibrary.length; i++){
     if(titleText === myLibrary[i].title){
       let text = complete.innerText;
-      if(text === "completed"){
+      if(text === "Completed"){
     text = "Not read yet";
     complete.innerText = text;
   } else{
-    text = "completed";
+    text = "Completed";
     complete.innerText = text;
   } //console.log(i);
     //console.log(text);
@@ -76,7 +80,7 @@ if(read){
  const remove = document.createElement('button');
 remove.classList.add("remove");
 remove.innerHTML = "remove";
-document.getElementById("books").appendChild(remove);
+document.getElementsByClassName("bookDiv")[indexOfClass].appendChild(remove);
  remove.addEventListener("click", ()=>{
    let titleText = bookTitle.innerText;
   for(let i = 0; i < myLibrary.length; i++){
@@ -91,3 +95,5 @@ document.getElementById("books").appendChild(remove);
   }
  });
 }
+console.log(document.getElementById("books"));
+console.log(myLibrary);
